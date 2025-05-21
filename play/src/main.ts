76c74d7ceac329fb20af1b38b9d6739a4758e233
mixed <1919600547@qq.com>
2025-05-21 13:14:41 +0800

    ✨ feat(theme): 更新主题变量，调整颜色和边框样式

--- play/src/main.ts
+++ play/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@fan-ui/theme-chalk/src/dark/css-vars.scss'
 import '@fan-ui/theme-chalk/src/index.scss'
 import install from '@fan-ui/components'
 const app = createApp(App)
