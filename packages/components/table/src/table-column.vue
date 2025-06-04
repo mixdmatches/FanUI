8e7c56884c09d5d0401c0f4eb091011c055b8089
mixed <1919600547@qq.com>
2025-06-04 15:06:45 +0800

    ✨ feat(table): 新增FTable组件及其样式，包含表格和表格列的基本属性和结构

---
+++ packages/components/table/src/table-column.vue
@@ -0,0 +1,16 @@
+<template>
+  <div :class="bem.b()"></div>
+</template>
+
+<script setup lang="ts">
+import { createNamespace } from '@fan-ui/utils'
+import { tableColumnProps } from './table-column'
+
+defineOptions({
+  name: 'f-table-column'
+})
+
+const bem = createNamespace('table-column')
+
+defineProps(tableColumnProps)
+</script>
