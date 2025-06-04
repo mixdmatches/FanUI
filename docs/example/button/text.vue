c4213c0e4a170ac1a07c43e700a2eaa73da4ece5
mixed <1919600547@qq.com>
2025-06-04 15:47:01 +0800

    📝 docs(button): 更新按钮示例，添加不同类型和状态的按钮，优化文档展示

--- docs/example/button/text.vue
+++ docs/example/button/text.vue
@@ -1,18 +1,10 @@
 <template>
-  <div class="mb-10">
-    <f-button text>default</f-button>
-    <f-button text type="primary">primary</f-button>
-    <f-button text type="success">success</f-button>
-    <f-button text type="info">info</f-button>
-    <f-button text type="warning">warning</f-button>
-    <f-button text type="danger">danger</f-button>
-  </div>
-  <div class="mb-10">
-    <f-button disabled text plain>plain</f-button>
-    <f-button disabled text type="primary" plain>primary</f-button>
-    <f-button disabled text type="success" plain>success</f-button>
-    <f-button disabled text type="info" plain>info</f-button>
-    <f-button disabled text type="warning" plain>warning</f-button>
-    <f-button disabled text type="danger" plain>danger</f-button>
-  </div>
+  <f-button text>default</f-button>
+  <f-button text type="primary" plain>primary</f-button>
+  <f-button text type="success" round>success</f-button>
+  <f-button text type="info" circle :icon="Mail"></f-button>
 </template>
+
+<script lang="ts" setup>
+import { Mail } from '@icon-park/vue-next'
+</script>
