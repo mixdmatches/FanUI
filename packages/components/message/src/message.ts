8ce416c6e2c58ef98151e89011a6678eb872db6c
mixed <1919600547@qq.com>
2025-10-09 16:56:26 +0800

    refactor(message): 重构消息组件，提取消息属性到单独文件并修正导入路径

--- packages/components/message/src/message.ts
+++ packages/components/message/src/message.ts
@@ -1,36 +1,59 @@
-import type { ExtractPropTypes } from 'vue'
+import { Message } from '@icon-park/vue-next'
+import { MessageProps } from './props'
+import { h, render } from 'vue'
 
-const MESSAGE_TYPE = ['success', 'info', 'warning', 'error']
+let messageInstanceList: Array<HTMLElement> = []
 
-export const messageProps = {
-  /** @description 消息类型 */
-  type: {
-    type: String,
-    default: 'info',
-    validator(value: string) {
-      return MESSAGE_TYPE.includes(value)
+const MESSAGE_START_TOP = 56 //起始高度
+const MESSAGE_HEIGHT = 44 // 消息高度
+const MESSAGE_GAP = 16 // 消息间距
+
+const destroyMessageElement = (element: HTMLElement) => {
+  if (!element.parentElement?.contains(element)) return
+  element.parentElement.removeChild(element)
+  messageInstanceList = messageInstanceList.filter(item => item !== element)
+  messageInstanceList.forEach((item, index) => {
+    item.style.top = `${MESSAGE_START_TOP + MESSAGE_HEIGHT * index + MESSAGE_GAP * index}px`
+  })
+}
+
+const setDestroyClock = (element: HTMLElement, time = 3000) => {
+  if (time === 0) return
+  setTimeout(() => {
+    destroyMessageElement(element)
+  }, time)
+}
+
+function message(config: MessageProps) {
+  const VNode = h(Message, {
+    ...config,
+    onClose(element: HTMLElement) {
+      destroyMessageElement(element) // 关闭时销毁
     }
-  },
-  /** @description 消息内容 */
-  message: {
-    type: String,
-    default: ''
-  },
-  /** @description 控制是否显示关闭按钮 */
-  showClose: {
-    type: Boolean,
-    default: false
-  },
-  /** @description 显示时间 */
-  duration: {
-    type: Number,
-    default: 3000
-  },
-  /** @description plain背景 */
-  plain: {
-    type: Boolean,
-    default: false
-  }
-} as const
+  })
+  const container = document.createElement('div')
+  container.setAttribute('class', 'f-message-container')
+  document.body.append(container)
+  messageInstanceList.push(container)
+  container.style.top =
+    MESSAGE_START_TOP +
+    (messageInstanceList.length - 1) * (MESSAGE_HEIGHT + MESSAGE_GAP) +
+    'px'
+  render(VNode, container)
+  setDestroyClock(
+    container,
+    config.duration !== undefined ? config.duration : 3000
+  )
+}
 
-export type MessageProps = Partial<ExtractPropTypes<typeof messageProps>>
+const FMessage = (config: MessageProps | string) => {
+  if (typeof config === 'string') config = { message: config } as MessageProps
+  message(config)
+}
+
+FMessage.success = (msg: string) => message({ message: msg, type: 'success' })
+FMessage.error = (msg: string) => message({ message: msg, type: 'error' })
+FMessage.info = (msg: string) => message({ message: msg, type: 'info' })
+FMessage.warning = (msg: string) => message({ message: msg, type: 'warning' })
+
+export { FMessage }
---
+++ packages/components/message/src/props.ts
@@ -0,0 +1,36 @@
+import type { ExtractPropTypes } from 'vue'
+
+const MESSAGE_TYPE = ['success', 'info', 'warning', 'error']
+
+export const messageProps = {
+  /** @description 消息类型 */
+  type: {
+    type: String,
+    default: 'info',
+    validator(value: string) {
+      return MESSAGE_TYPE.includes(value)
+    }
+  },
+  /** @description 消息内容 */
+  message: {
+    type: String,
+    default: ''
+  },
+  /** @description 控制是否显示关闭按钮 */
+  showClose: {
+    type: Boolean,
+    default: false
+  },
+  /** @description 显示时间 */
+  duration: {
+    type: Number,
+    default: 3000
+  },
+  /** @description plain背景 */
+  plain: {
+    type: Boolean,
+    default: false
+  }
+} as const
+
+export type MessageProps = Partial<ExtractPropTypes<typeof messageProps>>
